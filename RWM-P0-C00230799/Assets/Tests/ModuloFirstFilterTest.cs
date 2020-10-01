d25a07b7f18e7da6587303f05d86803008b79d10
MateuszStepien0 <c00230799@itcarlow.ie>
2020-10-01 13:48:27 +0100

    committing unit tests

---
+++ RWM-P0-C00230799/Assets/Tests/ModuloFirstFilterTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ModuloFirstFilterTest
+    {
+        [Test]
+        public void ModuloFirstFilterSimple()
+        {
+            int[] input = { 4, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
+            int[] output = ModuloFirstFilter.results(input);
+            int[] expected = { 0, 2, 0, 3, 1, 0, 2, 1, 3, 2 };
+
+            foreach (var o in output)
+            {
+                Debug.Log(o);
+            }
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void ModuloFirstFilterNegative()
+        {
+            int[] input = { -4, -1, -5, -2, -7, -5, -2, -8, -10, -14 };
+            int[] output = ModuloFirstFilter.results(input);
+            int[] expected = { 0, -1, -1, -2, -3, -1, -2, 0, -2, -2 };
+
+            foreach (var o in output)
+            {
+                Debug.Log(o);
+            }
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
